89552e7fcd544df911e3935a82c81da5ed516831
zhao1fanethan@gmail.com <zhao1fanethan@gmail.com>
2025-04-23 00:28:01 +0000

    Created BookListForm.ItemTemplate1 and ServerModule1; Edited BookDetailForm, BookForm, BookListForm, MyFavouritesForm and 1 more

--- client_code/BookListForm/ItemTemplate1/__init__.py
+++ client_code/BookListForm/ItemTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import BookFormTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,9 +7,12 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class BookForm(BookFormTemplate):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def details_button_click(self, **event_args):
+    open_form('BookDetailForm', book=self.item)
--- client_code/BookListForm/__init__.py
+++ client_code/BookListForm/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import BookListFormTemplate
 from anvil import *
 import anvil.users
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,5 +11,7 @@ class BookListForm(BookListFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+    self.repeating_panel_books.items = anvil.server.call('get_books')
 
     # Any code you write here will run before the form opens.
