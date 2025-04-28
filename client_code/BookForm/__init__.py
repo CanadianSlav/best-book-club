1e438a3a365548edcf6261b1ae69dbf5cb8a9281
yehonathan10playsroblox@gmail.com <yehonathan10playsroblox@gmail.com>
2025-04-28 17:13:47 +0000

    Edited BookForm, BookListForm and MyFavouritesForm; Deleted BookListForm_copy

--- client_code/BookForm/__init__.py
+++ client_code/BookForm/__init__.py
@@ -8,50 +8,67 @@ from anvil.tables import app_tables
 
 
 class BookForm(BookFormTemplate):
-  def __init__(self, book=None, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, book=None, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        
+        self.book = book
     
-    self.book = book
+        # If editing an existing book, fill in the fields
+        if self.book:
+            self.text_box_title.text = self.book['title']
+            self.text_box_author.text = self.book['author']
+            self.text_box_genre.text = self.book['genre']
+            self.text_area_summary.text = self.book['summary']
+            # can't set file directly to FileLoader, so just leave it empty
+    
+    def file_loader_cover_change(self, file, **event_args):
+        # You don't need to set anything here unless you want a preview
+        pass
+    
+    def button_submit_click(self, **event_args):
+        # Figure out which cover image to use
+        cover_image = self.file_loader_cover_image.file
+        if not cover_image and self.book:
+            cover_image = self.book['cover_image']
+    
+        if self.book:
+        # Update the existing book
+            anvil.server.call('update_book',
+                            book=self.book,
+                            title=self.text_box_title.text,
+                            author=self.text_box_author.text,
+                            genre=self.text_box_genre.text,
+                            summary=self.text_area_summary.text,
+                            cover_image=cover_image)
+        else:
+        # Add a new book
+            anvil.server.call('add_new_book',
+                            title=self.text_box_title.text,
+                            author=self.text_box_author.text,
+                            genre=self.text_box_genre.text,
+                            summary=self.text_area_summary.text,
+                            cover_image=cover_image)
+    
+        # After adding/updating, go back to the book list
+        open_form('BookListForm')
+    
+    def button_cancel_click(self, **event_args):
+        open_form('BookListForm')
 
-    # If editing an existing book, fill in the fields
-    if self.book:
-      self.text_box_title.text = self.book['title']
-      self.text_box_author.text = self.book['author']
-      self.text_box_genre.text = self.book['genre']
-      self.text_area_summary.text = self.book['summary']
-      # can't set file directly to FileLoader, so just leave it empty
 
-  def file_loader_cover_change(self, file, **event_args):
-    # You don't need to set anything here unless you want a preview
-    pass
+    def button_all_books_click(self, **event_args):
+        open_form("BookListForm")
 
-  def button_submit_click(self, **event_args):
-    # Figure out which cover image to use
-    cover_image = self.file_loader_cover_image.file
-    if not cover_image and self.book:
-      cover_image = self.book['cover_image']
+    def favourite_books_button_click(self, **event_args):
+        open_form("MyFavouritesForm")
 
-    if self.book:
-      # Update the existing book
-      anvil.server.call('update_book',
-                        book=self.book,
-                        title=self.text_box_title.text,
-                        author=self.text_box_author.text,
-                        genre=self.text_box_genre.text,
-                        summary=self.text_area_summary.text,
-                        cover_image=cover_image)
-    else:
-      # Add a new book
-      anvil.server.call('add_new_book',
-                        title=self.text_box_title.text,
-                        author=self.text_box_author.text,
-                        genre=self.text_box_genre.text,
-                        summary=self.text_area_summary.text,
-                        cover_image=cover_image)
+    def add_article_button_click(self, **event_args):
+        open_form("BookForm")
 
-    # After adding/updating, go back to the book list
-    open_form('BookListForm')
+    def login_button_click(self, **event_args):
+        anvil.users.login_with_form()
 
-  def button_cancel_click(self, **event_args):
-    open_form('BookListForm')
+    def button_logout_click(self, **event_args):
+        anvil.users.logout()
+        open_form("BookListForm")
--- client_code/MyFavouritesForm/__init__.py
+++ client_code/MyFavouritesForm/__init__.py
@@ -8,30 +8,41 @@ from anvil.tables import app_tables
 
 
 class MyFavouritesForm(MyFavouritesFormTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    user = anvil.users.get_user()
-    if not user:
-        alert("You must be logged in to view your favorite books.")
-        open_form('BookListForm')  # Send them back to book list
-        return
-
-    no_favourites = anvil.server.call('no_favourites')
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
     
-    if no_favourites:
-        alert("You must favourite a book first.")
-        open_form('BookListForm')  # Send them back to book list
-        return
-    # Any code you write here will run before the form opens.
+        user = anvil.users.get_user()
+        if not user:
+            alert("You must be logged in to view your favorite books.")
+            open_form('BookListForm')  # Send them back to book list
+            return
+    
+        no_favourites = anvil.server.call('no_favourites')
+        
+        if no_favourites:
+            alert("You must favourite a book first.")
+            open_form('BookListForm')  # Send them back to book list
+            return
+
+        self.repeating_panel_favourites.items = anvil.server.call('get_favorite_books')
+        
+        # Any code you write here will run before the form opens.
 
       
-    self.repeating_panel_favourites.items = anvil.server.call('get_favorite_books')
 
+    def button_all_books_click(self, **event_args):
+        open_form("BookListForm")
 
+    def favourite_books_button_click(self, **event_args):
+        open_form("MyFavouritesForm")
 
-  def back_button_click(self, **event_args):
-    open_form('BookListForm')
+    def add_article_button_click(self, **event_args):
+        open_form("BookForm")
 
- 
+    def login_button_click(self, **event_args):
+        anvil.users.login_with_form()
+
+    def button_logout_click(self, **event_args):
+        anvil.users.logout()
+        open_form("BookListForm")
